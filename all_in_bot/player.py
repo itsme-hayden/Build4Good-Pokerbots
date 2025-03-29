07f793cb1b11244eb6571e35d7893e06b12c42c0
jbb248 <joebray248@outlook.com>
2025-03-29 11:47:52 -0500

    Push functions

--- all_in_bot/player.py
+++ all_in_bot/player.py
@@ -11,6 +11,9 @@ import random
 
 
 class Player(Bot):
+
+    big_blind:bool = False
+
     '''
     A pokerbot.
     '''
@@ -39,11 +42,11 @@ class Player(Bot):
         Returns:
         Nothing.
         '''
-        #my_bankroll = game_state.bankroll  # the total number of chips you've gained or lost from the beginning of the game to the start of this round
-        #game_clock = game_state.game_clock  # the total number of seconds your bot has left to play this game
-        #round_num = game_state.round_num  # the round number from 1 to NUM_ROUNDS
-        #my_cards = round_state.hands[active]  # your cards
-        #big_blind = bool(active)  # True if you are the big blind
+        my_bankroll = game_state.bankroll  # the total number of chips you've gained or lost from the beginning of the game to the start of this round
+        game_clock = game_state.game_clock  # the total number of seconds your bot has left to play this game
+        round_num = game_state.round_num  # the round number from 1 to NUM_ROUNDS
+        my_cards = round_state.hands[active]  # your cards
+        self.big_blind = bool(active)  # True if you are the big blind
         pass
 
     def handle_round_over(self, game_state, terminal_state, active):
@@ -58,11 +61,23 @@ class Player(Bot):
         Returns:
         Nothing.
         '''
-        #my_delta = terminal_state.deltas[active]  # your bankroll change from this round
+        my_delta = terminal_state.deltas[active]  # your bankroll change from this round
         previous_state = terminal_state.previous_state  # RoundState before payoffs
-        #street = previous_state.street  # 0, 3, 4, or 5 representing when this round ended
-        #my_cards = previous_state.hands[active]  # your cards
-        #opp_cards = previous_state.hands[1-active]  # opponent's cards or [] if not revealed
+        street = previous_state.street  # 0, 3, 4, or 5 representing when this round ended
+        my_cards = previous_state.hands[active]  # your cards
+        opp_cards = previous_state.hands[1-active]  # opponent's cards or [] if not revealed
+        pass
+
+    def check_straight(self, hand):
+        pass
+
+    def check_four_kind(self, hand):
+        pass
+
+    def check_three_kind(self, hand):
+        pass
+
+    def check_pair(self, hand):
         pass
 
     def get_action(self, game_state, round_state, active):
@@ -90,6 +105,14 @@ class Player(Bot):
         my_contribution = STARTING_STACK - my_stack  # the number of chips you have contributed to the pot
         opp_contribution = STARTING_STACK - opp_stack  # the number of chips your opponent has contributed to the pot
 
+        # Cool Decision Making B)
+
+        # If we just started and we call first: raise ten to guess behavior
+        if street == 0 and my_pip < 10:
+            return RaiseAction(10)
+        
+        straight = check_straight()
+
         if RaiseAction in legal_actions:
            min_raise, max_raise = round_state.raise_bounds()  # the smallest and largest numbers of chips for a legal bet/raise
            min_cost = min_raise - my_pip  # the cost of a minimum bet/raise
@@ -98,9 +121,10 @@ class Player(Bot):
             return RaiseAction(random.randint(min_cost, max_cost))
         if CheckAction in legal_actions:  # check-call
             return CheckAction()
-        if random.random() < 0.25:
+        if random.random() < 0.25: 
             return FoldAction()
         return CallAction()
+
 
 
 if __name__ == '__main__':
